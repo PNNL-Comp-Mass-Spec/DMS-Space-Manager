4472baec36769a3fb0da82218a02aab892a7aee5
Matthew Monroe <matthew.monroe@pnnl.gov>
2012-04-03 19:18:37 +0000

    Now using .DriveInfo object when checking a local drive (perspective = server) Now checking for WMI returning null for "FreeSpace"; this indicates the drive is not ready or not found

--- DMS_Space_Manager/clsUtilityMethods.cs
+++ DMS_Space_Manager/clsUtilityMethods.cs
@@ -99,28 +99,116 @@ namespace Space_Manager
             public static SpaceCheckResults IsPurgeRequired(string machine, string perspective, clsDriveData driveData, out double driveFreeSpaceGB)
 			{
 				double availableSpace = 0;
-				string requestStr;
 				SpaceCheckResults testResult = SpaceCheckResults.Error;
 				// Set client/server flag based on config
 				bool client = perspective == "client" ? true : false;
                 driveFreeSpaceGB = -1;
-
-				// Get WMI object representing drive
+                
 				if (client)
 				{
-					requestStr = @"\\" + machine + @"\root\cimv2:win32_logicaldisk.deviceid=""" + driveData.DriveLetter + "\"";
-				}
+                    // Checking a remote drive
+                    // Get WMI object representing drive
+                    string requestStr;
+                    requestStr = @"\\" + machine + @"\root\cimv2:win32_logicaldisk.deviceid=""" + driveData.DriveLetter + "\"";
+
+                    try
+                    {
+                        ManagementObject disk = new ManagementObject(requestStr);
+                        disk.Get();
+                        
+                        object oFreeSpace = disk["FreeSpace"];
+                        double totalSpace;
+                        if (oFreeSpace == null)
+                        {
+                            string msg = "Drive " + driveData.DriveLetter + " not found via WMI; likely is Not Ready";
+                            clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogDb, clsLogTools.LogLevels.ERROR, msg);
+                            return SpaceCheckResults.Error;
+                        }
+                        else
+                        {
+                            availableSpace = System.Convert.ToDouble(oFreeSpace);
+                            totalSpace = System.Convert.ToDouble(disk["Size"]);
+                        }
+                       
+                        if (totalSpace <= 0)
+                        {
+                            string msg = "Drive " + driveData.DriveLetter + " reports a total size of 0 bytes via WMI; likely is Not Ready";
+                            clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogDb, clsLogTools.LogLevels.ERROR, msg);
+                            return SpaceCheckResults.Error;
+                        }
+                        else
+                        {
+                            driveFreeSpaceGB = availableSpace / Math.Pow(2D, 30D);	// Convert to GB
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        string msg = "Exception getting free disk space using WMI, drive " + driveData.DriveLetter + ": " + ex.Message;
+                        clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogDb, clsLogTools.LogLevels.ERROR, msg);
+                        testResult = SpaceCheckResults.Error;
+                        if (driveFreeSpaceGB > 0)
+                            driveFreeSpaceGB = -driveFreeSpaceGB;
+                        if (driveFreeSpaceGB == 0)
+                            driveFreeSpaceGB = -1;
+                    }
+
+                }
 				else
 				{
-					requestStr = "win32_logicaldisk.deviceid=\"" + driveData.DriveLetter + "\"";
+                    // Analyzing a drive local to this manager
+
+                    try
+                    {
+                        // Note: WMI string would be: "win32_logicaldisk.deviceid=\"" + driveData.DriveLetter + "\"";
+                        // Instantiate a new drive info object
+                        System.IO.DriveInfo diDrive = new System.IO.DriveInfo(driveData.DriveLetter);
+
+                        if (!diDrive.IsReady)
+                        {
+                            string msg = "Drive " + driveData.DriveLetter + " reports Not Ready via DriveInfo object; drive is offline or drive letter is invalid";
+                            clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogDb, clsLogTools.LogLevels.ERROR, msg);
+                            return SpaceCheckResults.Error;
+                        }
+                        else
+                        {
+                            if (diDrive.TotalSize <= 0)
+                            {
+                                string msg = "Drive " + driveData.DriveLetter + " reports a total size of 0 bytes via DriveInfo object; likely is Not Ready";
+                                clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogDb, clsLogTools.LogLevels.ERROR, msg);
+                                return SpaceCheckResults.Error;
+                            }
+                            else
+                            {
+                                driveFreeSpaceGB = diDrive.TotalFreeSpace / Math.Pow(2D, 30D);	// Convert to GB
+                            }
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+                        string msg = "Exception getting free disk space via .NET DriveInfo object, drive " + driveData.DriveLetter + ": " + ex.Message;
+                        clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogDb, clsLogTools.LogLevels.ERROR, msg);
+                        testResult = SpaceCheckResults.Error;
+                        if (driveFreeSpaceGB > 0)
+                            driveFreeSpaceGB = -driveFreeSpaceGB;
+                        if (driveFreeSpaceGB == 0)
+                            driveFreeSpaceGB = -1;
+                    }
+
 				}
 
-				try
-				{
-					ManagementObject disk = new ManagementObject(requestStr);
-					disk.Get();
-					availableSpace = System.Convert.ToDouble(disk["FreeSpace"]);
-                    driveFreeSpaceGB = availableSpace / Math.Pow(2D, 30D);	// Convert to GB
+                if (driveFreeSpaceGB < 0)
+                {
+                    testResult = SpaceCheckResults.Error;
+
+                    // Log space requirement if debug logging enabled
+                    string spaceMsg = "Drive " + driveData.DriveLetter + " Space Threshold: " + driveData.MinDriveSpace.ToString() +
+                        ", Drive not found";
+                    clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.DEBUG, spaceMsg);
+
+                }
+                else
+                {
                     if (driveFreeSpaceGB > driveData.MinDriveSpace)
                     {
                         testResult = SpaceCheckResults.Above_Threshold;
@@ -129,18 +217,13 @@ namespace Space_Manager
                     {
                         testResult = SpaceCheckResults.Below_Threshold;
                     }
-				}
-				catch (Exception ex)
-				{
-					string msg = "Exception getting free disk space, drive " + driveData.DriveLetter + ". Message = " + ex.Message;
-					clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.ERROR, msg);
-					testResult = SpaceCheckResults.Error;
-				}
 
-				// Log space requirement if debug logging enabled
-				string spaceMsg = "Drive " + driveData.DriveLetter + " Space Threshold: " + driveData.MinDriveSpace.ToString() +
-                    ", Avail space: " + driveFreeSpaceGB.ToString("####0.0");
-				clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.DEBUG, spaceMsg);
+                    // Log space requirement if debug logging enabled
+                    string spaceMsg = "Drive " + driveData.DriveLetter + " Space Threshold: " + driveData.MinDriveSpace.ToString() +
+                        ", Avail space: " + driveFreeSpaceGB.ToString("####0.0");
+                    clsLogTools.WriteLog(clsLogTools.LoggerTypes.LogFile, clsLogTools.LogLevels.DEBUG, spaceMsg);
+
+                }
 
 				return testResult;
 			}	// End sub
