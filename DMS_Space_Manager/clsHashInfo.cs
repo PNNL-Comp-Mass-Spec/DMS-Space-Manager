a733865dbe6f270d7391d5d5326e7576c4790f39
alchemistmatt <matthew.monroe@pnnl.gov>
2016-11-07 21:05:40 -0800

    Tabs to spaces

--- DMS_Space_Manager/clsHashInfo.cs
+++ DMS_Space_Manager/clsHashInfo.cs
@@ -1,77 +1,77 @@
 ﻿
 namespace Space_Manager
 {
-	public class clsHashInfo
-	{
-		private string mHashCode;
+    public class clsHashInfo
+    {
+        private string mHashCode;
         private string mMyEMSLFileID;
 
-		/// <summary>
-		/// MD5 or Sha-1 Hash
-		/// </summary>
-		public string HashCode
-		{
-			get
-			{
-				return mHashCode;
-			}
-			set
-			{
-				mHashCode = value ?? string.Empty;
-			}
-		}
+        /// <summary>
+        /// MD5 or Sha-1 Hash
+        /// </summary>
+        public string HashCode
+        {
+            get
+            {
+                return mHashCode;
+            }
+            set
+            {
+                mHashCode = value ?? string.Empty;
+            }
+        }
 
-		public string MyEMSLFileID
-		{
-			get
-			{
-				return mMyEMSLFileID;
-			}
-			set
-			{
-				mMyEMSLFileID = value ?? string.Empty;
-			}
-		}
+        public string MyEMSLFileID
+        {
+            get
+            {
+                return mMyEMSLFileID;
+            }
+            set
+            {
+                mMyEMSLFileID = value ?? string.Empty;
+            }
+        }
 
-		// Constructor
-		public clsHashInfo() :
-			this(string.Empty, string.Empty) { }
+        // Constructor
+        public clsHashInfo() :
+            this(string.Empty, string.Empty) { }
 
-		public clsHashInfo(string hashCode, string myEmslFileID)
-		{
-			this.Clear();
-			this.HashCode = hashCode;
-			this.MyEMSLFileID = myEmslFileID;
-		}
+        public clsHashInfo(string hashCode, string myEmslFileID)
+        {
+            this.Clear();
+            this.HashCode = hashCode;
+            this.MyEMSLFileID = myEmslFileID;
+        }
 
-		public void Clear()
-		{
-			HashCode = string.Empty;
-			MyEMSLFileID = string.Empty;
-		}
+        public void Clear()
+        {
+            HashCode = string.Empty;
+            MyEMSLFileID = string.Empty;
+        }
 
-		public bool IsMatch(clsHashInfo comparisonValue)
-		{
-			if (string.Equals(this.HashCode, comparisonValue.HashCode) &&
-				string.Equals(this.MyEMSLFileID, comparisonValue.MyEMSLFileID))
-				return true;
-			else
-				return false;
-		}
+        public bool IsMatch(clsHashInfo comparisonValue)
+        {
+            if (string.Equals(this.HashCode, comparisonValue.HashCode) &&
+                string.Equals(this.MyEMSLFileID, comparisonValue.MyEMSLFileID))
+                return true;
+            else
+                return false;
+        }
 
-		public override string ToString()
-		{
-			string description;
-			if (string.IsNullOrEmpty(HashCode))
-				description = "#No Hash#";
-			else
-				description = HashCode;
+        public override string ToString()
+        {
+            string description;
+            if (string.IsNullOrEmpty(HashCode))
+                description = "#No Hash#";
+            else
+                description = HashCode;
 
-			if (!string.IsNullOrEmpty(MyEMSLFileID))
-				description += ", ID=" + MyEMSLFileID;
+            if (!string.IsNullOrEmpty(MyEMSLFileID))
+                description += ", ID=" + MyEMSLFileID;
 
-			return description;
-		}
+            return description;
+        }
 
-	}
+    }
 }
