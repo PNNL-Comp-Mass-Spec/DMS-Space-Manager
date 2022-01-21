17383f3d41e4488e34a73cdb6ab93a388b9a443c
alchemistmatt <matthew.monroe@pnnl.gov>
2022-01-20 17:39:26 -0800

    Updated PRISM.dll from NuGet
    Creates log files named using year-month-day

--- DMS_Space_Manager/Program.cs
+++ DMS_Space_Manager/Program.cs
@@ -16,7 +16,7 @@ namespace Space_Manager
     /// </summary>
     internal static class Program
     {
-        private const string PROGRAM_DATE = "November 12, 2021";
+        private const string PROGRAM_DATE = "January 20, 2022";
 
         private static clsMainProgram mMainProgram;
 
