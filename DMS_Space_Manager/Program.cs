c6a4514b58a7334797720679d35a9683d10a9c2d
alchemistmatt <matthew.monroe@pnnl.gov>
2023-01-07 17:41:25 -0800

    Update MyEMSL Reader

--- DMS_Space_Manager/Program.cs
+++ DMS_Space_Manager/Program.cs
@@ -16,7 +16,7 @@ namespace Space_Manager
     /// </summary>
     internal static class Program
     {
-        private const string PROGRAM_DATE = "September 9, 2022";
+        private const string PROGRAM_DATE = "January 7, 2023";
 
         private static MainProgram mMainProgram;
 
